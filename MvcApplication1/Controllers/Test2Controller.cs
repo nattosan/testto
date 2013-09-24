0f2cc50d0356b7a1062d656d725995d1bd484baa
nattosan <natapon.me@gmail.com>
2013-09-24 16:25:20 +0700

    3deekao

---
+++ MvcApplication1/Controllers/Test2Controller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class Test2Controller : Controller
+    {
+        //
+        // GET: /Test2/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
